8bd65b754a6138646d1d0a3b8485fd085d417259
Patrick Brandt <patrick@fullstory.com>
2018-12-24 10:10:59 -0500

    fixing config test

--- config/config_test.go
+++ config/config_test.go
@@ -5,41 +5,38 @@ import (
 
 )
 
-func makeConf(tableSchema string) *Config {
-	conf := &Config{
-		Redshift: RedshiftConfig {
-			TableSchema: tableSchema,
-		},
+func makeValidator(tableSchema string) *Validator {
+	return &Validator{
+		TableSchema: tableSchema,
 	}
-	return conf
 }
 
 func TestValidateTableSchemaConfig(t *testing.T) {
 
 	testCases := []struct {
-		conf       *Config
+		validator       *Validator
 		hasError   bool
 		errMessage string
 	}{
 		{
-			conf:       makeConf(""),
+			validator:       makeValidator(""),
 			hasError:   true,
 			errMessage: "TableSchema definition missing from Redshift configuration. More information: https://www.hauserdocs.io",
 		},
 		{
-			conf:       makeConf("test"),
+			validator:       makeValidator("test"),
 			hasError:   false,
 			errMessage: "",
 		},
 		{
-			conf:       makeConf("search_path"),
+			validator:       makeValidator("search_path"),
 			hasError:   false,
 			errMessage: "",
 		},
 	}
 
 	for _, tc := range testCases {
-		err := tc.conf.Redshift.ValidateTableSchema()
+		err := tc.validator.ValidateTableSchema()
 		if tc.hasError && err == nil {
 			t.Errorf("expected Redshift.ValidateTableSchema() to return an error when config.Config.Redshift.TableSchema is empty")
 		}
@@ -47,7 +44,7 @@ func TestValidateTableSchemaConfig(t *testing.T) {
 			t.Errorf("expected Redshift.ValidateTableSchema() to return \n%s \nwhen config.Config.Redshift.TableSchema is empty, returned \n%s \ninstead", tc.errMessage, err)
 		}
 		if !tc.hasError && err != nil {
-			t.Errorf("unexpected error thrown for TableSchema %s: %s", tc.conf.Redshift.TableSchema, err)
+			t.Errorf("unexpected error thrown for TableSchema %s: %s", tc.validator.TableSchema, err)
 		}
 	}
 }
